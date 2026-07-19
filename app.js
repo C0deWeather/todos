5861c22223233ebd41765fc136e9e34ece94ec95
Bright Frank <frankbright211@gmail.com>
2026-07-19 03:15:34 +0100

    Initialize Express app with error handling

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import router from './routes.js';
+
+const app = express();
+const port = 3000;
+
+ap.use(express.json())
+
+app.use(router);
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message,
+      status: err.status || 500
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`todos app listening on port ${port}`)
+});
