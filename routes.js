ab83b4bddf511dad7af8c586e1305205cfb023a0
Bright Frank <frankbright211@gmail.com>
2026-07-19 03:46:02 +0100

    Initialize routes.js with express router

---
+++ routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ValidationError, NotFoundError } from 'errors.js';
+
+const router = express.Router();
+
+router.post();
+router.get();
+router.get();
+router.patch();
+router.delete();
+
+export default router;
