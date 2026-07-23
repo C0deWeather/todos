49a0d6b791da00efaa4c1641c57ef9271be8a384
Bright Frank <frankbright211@gmail.com>
2026-07-23 01:50:33 +0100

    Fix task update and implement deleteTask function

--- data.js
+++ data.js
@@ -15,13 +15,26 @@ export function getTasks() {
   return structuredClone(tasks);
 }
 
-export function getTaskByID(id) {
+export function getTaskById(id) {
   const task = tasks.find(task => task.id === id);
   if (!task) {
     throw new NotFoundError("task not found");
   }
   return task;
 
-  
-    
-  
+export function updateTask(id, updatedTask) {
+  const currentTask = tasks.find(t => t.id === id);
+  if (!currentTask) {
+    throw new NotFoundError("task not found");
+  }
+  currentTask.title = updatedTask.title;
+  currentTask.status = updatedTask.status;
+}
+
+export function deleteTask(id) {
+  indexInArray = tasks.findIndex(t => t.id === id);
+  if (!indexInArray) {
+    throw new NotFoundError("task not found");
+  }
+  tasks.splice(indexInArray, 1);
+}
